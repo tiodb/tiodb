bf6a6df5b090c0a8f1fc6fe29ae5bbef64de7b9e
Rodrigo Strauss <rodrigostrauss@gmail.com>
2010-05-25 11:06:42 -0300

    cluster examples created

---
+++ examples/cluster_factorial_processor.py
@@ -0,0 +1,19 @@
+import tioclient
+tio = tioclient.Connect('tio://127.0.0.1:12345')
+source = tio.CreateContainer('factorial_cluster/source', 'volatile/list')
+
+destination_name = source.get_property('destination')
+destination = tio.OpenContainer(destination_name)
+
+def factorial(n):
+	if n == 0: return 1
+	return n * factorial(n -1)
+
+def GottaWorkToDo(container, event_name, key, value, metadata):
+	print 'calculating factorial(%d)' % value
+	# like "'20!=2432902008176640000'"
+	destination.push_back('%d!=%s' % (value, factorial(value)))
+	source.wait_and_pop_next(GottaWorkToDo)
+
+source.wait_and_pop_next(GottaWorkToDo)
+tio.RunLoop()
---
+++ examples/cluster_factorial_producer.py
@@ -0,0 +1,23 @@
+import random
+import tioclient
+tio = tioclient.Connect('tio://127.0.0.1:12345')
+source = tio.CreateContainer('factorial_cluster/source', 'volatile/list')
+destination = tio.CreateContainer('factorial_cluster/destination', 'volatile/list')
+
+source.set_property('destination', 'factorial_cluster/destination')
+
+def OnItemDone(container, event_name, key, value, metadata):
+    print value
+    
+destination.subscribe(OnItemDone)
+
+# add 10k integers from 1 to 500
+item_count = 10 * 1000
+for x in range(item_count):
+    source.append(random.randint(1,500))
+    
+print 'generated %d items, now waiting for results' % item_count
+
+tio.RunLoop()
+
+	
