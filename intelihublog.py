873adf7450d4b6917360b84311a01dbb8e495373
Rodrigo Strauss <rodrigo.strauss@intelitrader.com.br>
2013-02-14 19:10:37 -0200

    intelihublog: real time playback (following a log file like tail -f), and delay support

--- intelihublog.py
+++ intelihublog.py
@@ -1,8 +1,9 @@
 import intelihubclient
 import sys
 import time
-from optparse import OptionParser
+import argparse
 import bz2
+
 
 
 class InteliHubLogParser(object):
@@ -32,10 +33,80 @@ class InteliHubLogParser(object):
 
   def register_container(self, handle, name, type):
     self.containers
+
+  def play_log_line(self, hub, container_dict, line):
+    # remove \r
+    if line[-1] == '\n':
+      line = line[:-1]
+      
+    fields = line.split(',', 4)
+
+    t, command, handle, key_info, rest = fields
+
+    size, key = self.deserialize(key_info, rest)
+    rest = rest[size+1:]
+    value_info, rest = rest.split(',',1)
+    size, value = self.deserialize(value_info, rest)
+
+    change = False
+    
+
+    if command == 'create':
+      # we should ignore special containers
+      if key.startswith('__'):
+        return False
+      c = hub.create(key, value)
+      c.clear()
+      container_dict[handle] = c
+    else:
+      change = True
+      c = container_dict[handle]
+      if command == 'push_back':
+        c.push_back(value)
+      elif command == 'push_front':
+        c.push_front(value)
+      elif command == 'pop_back':
+        c.pop_back()
+      elif command == 'pop_front':
+        c.pop_front()
+      elif command == 'set':
+        c.set(key, value)
+      elif command == 'insert':
+        c.insert(key, value)
+      elif command == 'delete':
+        c.delete(key)
+      elif command == 'clear':
+        c.clear()
+      elif command == 'propset':
+        c.propset(key, value)
+      elif command == 'group_add':
+        hub.group_add(key, value)
+      else:
+        raise Exception('unknown command ' + command)
+
+    return change
+
+  def wait_for_line(self, f):
+    sleep_time = 0.5
+    slept_time = 0
+    
+    while True:
+      line = f.readline()
+
+      if line:
+        return line
+
+      time.sleep(sleep_time)
+
+      slept_time += sleep_time
+
+      if slept_time % 5 == 0:
+        print '%d seconds waiting for file to grow...' % slept_time
     
   
-  def replay(self, hub, file_path):
+  def replay(self, hub, file_path, speed, delay_seconds=0, follow=False):
     containers = {}
+    self.speed = speed
 
     if file_path.endswith('bz2'):
         f = bz2.BZ2File(file_path)
@@ -50,10 +121,29 @@ class InteliHubLogParser(object):
       line = f.readline()
 
       if not line:
-        break
+        if not follow:
+          break
+        else:
+          line = self.wait_for_line(f)
+
+      if delay_seconds > 0:
+        now = int(time.time())
+        message_time = int(line.split(',')[0])
+        message_time += delay_seconds
+        delta = message_time - now
+
+        if delta > 0:
+          if delta > 10:
+            print 'sleep for %s seconds to apply a %s seconds delay' % (delta, delay_seconds)
+          time.sleep(delta)
 
       total_data += len(line)
       count += 1
+
+      changed = self.play_log_line(hub, containers, line)
+
+      if changed:
+        total_changes += 1
                
       log = False
       if self.speed == 0:
@@ -64,65 +154,36 @@ class InteliHubLogParser(object):
 
       if log:
         print count, len(containers), 'containers,', total_changes, 'changes,' , (total_data / 1024), 'kb so far'
-      
-
-      # remove \r
-      line = line[:-1]
-      fields = line.split(',', 4)
-
-      t, command, handle, key_info, rest = fields
-
-      size, key = self.deserialize(key_info, rest)
-      rest = rest[size+1:]
-      value_info, rest = rest.split(',',1)
-      size, value = self.deserialize(value_info, rest)
-      
-
-      if command == 'create':
-        c = hub.create(key, value)
-        c.clear()
-        containers[handle] = c
-      else:
-        total_changes += 1
-        c = containers[handle]
-        if command == 'push_back':
-          c.push_back(value)
-        elif command == 'push_front':
-          c.push_front(value)
-        if command == 'pop_back':
-          c.pop_back()
-        elif command == 'pop_front':
-          c.pop_front()
-        elif command == 'set':
-          c.set(key, value)
-        elif command == 'insert':
-          c.insert(key, value)
-        elif command == 'delete':
-          c.delete(key)
-        elif command == 'clear':
-          c.clear()
-        elif command == 'propset':
-          c.propset(key, value)
-        elif command == 'group_add':
-          hub.group_add(key, value)
-        else:
-          raise Exception('unknown command ' + command)
-          
-        if self.speed > 0:
-            time.sleep(1.0 / self.speed)
+     
+         
+      if self.speed > 0:
+          time.sleep(1.0 / self.speed)
         
 def main():
   parser = InteliHubLogParser()
-  hub_host, file_path, speed = sys.argv[1:]
-  speed = int(speed)
 
-  print 'Loading file "%s" to InteliHub @ %s, %d msgs/sec' % (file_path, hub_host, speed)
+  argparser = argparse.ArgumentParser('Replay InteliHub logs')
+  argparser.add_argument('hub')
+  argparser.add_argument('file_path')
+  argparser.add_argument('--speed', default=0, type=int)
+  argparser.add_argument('--delay', default=0, type=int)
+  argparser.add_argument('--follow', action='store_true')
+
+  params = argparser.parse_args()
+
+  print 'Loading file "%s" to InteliHub @ %s, %d msgs/sec, %d seconds delay %s' % \
+    (params.file_path, params.hub, params.speed,
+     params.delay, '(follow file)' if params.follow else '')
+   
+  hub = intelihubclient.connect(params.hub)
   
-  parser.speed = speed
-  
-  hub = intelihubclient.connect(hub_host)
-  
-  parser.replay(hub, file_path)
+  parser.replay(
+    hub,
+    params.file_path,
+    params.speed,
+    params.delay,
+    params.follow)
+    
 
 if __name__ == '__main__':
   main()
