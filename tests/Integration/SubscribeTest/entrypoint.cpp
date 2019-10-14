2e379f8fb6568ddde508cf9461342a84608b17ab
Alexsander Lima <alexsanderlimare@gmail.com>
2019-10-14 16:49:31 -0300

    Adjust minimum required version and add SubscribeTest

---
+++ tests/Integration/SubscribeTest/entrypoint.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+#include <thread>
+#include <chrono>
+#include "tioclient.h"
+#include "tioclient.hpp"
+#include "gmock/gmock.h"
+
+TEST(SubscribeTest, ReceiveCallbackFromSubscription)
+{
+	tio::Connection cn("127.0.0.1");
+	tio::containers::list<std::string> mycontainer;
+
+	mycontainer.create(&cn, "my_container", "volatile_list");
+	mycontainer.clear();
+
+	uint16_t eventsArrivedCount{};
+
+	mycontainer.subscribe(
+	[&eventsArrivedCount](const std::string& containerName, const std::string& eventName, const int& key, const std::string& value)
+	{
+		eventsArrivedCount++;
+	});
+
+	const uint16_t expectedEventsArrivedCount = 5;
+
+	for (uint16_t i = 1; i <= expectedEventsArrivedCount; ++i)
+	{
+		mycontainer.push_back("v_" + std::to_string(i));
+	}
+
+	cn.WaitForNextEventAndDispatch(1);
+
+	ASSERT_THAT(eventsArrivedCount, testing::Eq(expectedEventsArrivedCount));
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleMock(&argc, argv);
+
+	return RUN_ALL_TESTS();
+
+	return 0;
+}
